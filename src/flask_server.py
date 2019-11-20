e68ad6a3ae512c7e548b2dce15b98b7bc625a6a2
Patrick Li <pwang245@wisc.edu>
2019-11-19 16:26:15 -0800

    all tests passed wo makefile

--- src/flask_server.py
+++ src/flask_server.py
@@ -40,7 +40,7 @@ def create_instance_container():
     if 'major' not in json_obj:
         return "", 409
     result = manager.launch_container(json_obj)
-    json_obj["instance"] = result
+    json_obj["instance"] = str(result)
     return json_obj, 200
 
 
@@ -53,16 +53,24 @@ def get_running_instance_list():
 @app.route('/destroy/<instance>', methods=['DELETE'])
 def destroy_running_instance(instance):
     result = manager.get_instances()["instances"]
+    
     for ins in result:
-        if instance in ins:
-            result.remove(ins)
+        if ins["instance"] == instance:
+            manager.kill_one_container_process(instance)
+            # result.remove(ins)
             return "", 200
-    return "", 400
+    return "", 404
+    
 
 @app.route('/destroyall/', methods=['DELETE'])
 def destroy_all():
+    
     result = manager.get_instances()["instances"]
-    result.clear()
+    print(result)
+    for instance in result:
+        if "instance" in instance:
+            manager.kill_one_container_process(instance["instance"])
+    # result.clear()
     return "", 200
 
     
--- src/manager.py
+++ src/manager.py
@@ -1,13 +1,15 @@
 import configparser
-import os
+import os, signal
 import glob
 import os.path
 from os import path
 import tarfile
 import json
 from multiprocessing import Process
+import time
 
 instances = []
+processes = []
 
 class manager:
     
@@ -45,10 +47,12 @@ class manager:
         return json_obj
 
     def launch_container_process(port):
-        os.system('sudo chroot basefs /bin/bash -c "export PORT=' + port + ' && /webserver/tiny.sh"')
+        # os.system('sudo chroot basefs /bin/bash -c "export PORT=' + port + ' && /webserver/tiny.sh"')
+        os.system('unshare -p -f --mount-proc=$PWD/basefs/proc chroot basefs /bin/bash -c "export PORT=' + port + ' && /webserver/tiny.sh"')
 
     def launch_container(data):
         global instances
+        global processes
         config = configparser.RawConfigParser()
         
         cfgName = data["name"] + "-" + data["major"] + "-" + data["minor"] + ".cfg"
@@ -81,27 +85,33 @@ class manager:
                 desDirName = mountTarFileDes.split('/')[-1]   
             state = mountNoSpace[-1].split("'")[0]            #  READ
             print(state)
-            os.system("mkdir " + desDirName)
+            # os.system("mkdir " + desDirName)
             os.system("cp ../../mountables/" + mountTarFile + " ./")
-            os.system("mkdir -p ./basefs" + mountTarFileDes)
+            os.system("sudo mkdir ./basefs" + mountTarFileDes)
 
             with tarfile.open(mountTarFile) as tar1:
                 tar1.extractall(path=pathForContainer)
 
             if (state == "READ"):
+                print("inside READ !!")
                 os.system("sudo mount --bind -o ro $PWD/" + desDirName + " $PWD/basefs" + mountTarFileDes)
             elif (state == "READWRITE"):
                 os.system("sudo mount --bind -o rw $PWD/" + desDirName + " $PWD/basefs" + mountTarFileDes)
 
+        os.system('mount -t proc proc $PWD/basefs/proc')
+
         container_process = Process(target=manager.launch_container_process, args=(port, ))
         container_process.start()
-
-        data["instance"] = cfgName
-        instances.append(data)
+        os.setpgid(container_process.pid, container_process.pid)
+        processes.append(container_process)
+        data["instance"] = str(container_process.pid)
+        time.sleep(1)
+        result = container_process.pid
+        instances.append(data)   ## instances contain an array of jsons
 
         os.chdir("../../src")
 
-        return cfgName
+        return result
 
     def get_instances():
         global instances
@@ -110,4 +120,72 @@ class manager:
         }
         return result
 
-    
+    def kill_one_container_process(pid):
+        global processes
+        global instances
+        for process in processes:
+            if process.is_alive() and str(process.pid) == pid:
+                os.killpg(process.pid, signal.SIGKILL)
+                print(process.is_alive())
+                processes.remove(process)
+                break
+                # print("reached process termination")
+                
+        for instance in instances:
+            if instance["instance"] == pid:
+                sectionName = instance["name"] + "-" + instance["major"] + "-" + instance["minor"]
+                config = configparser.RawConfigParser()
+                cfgName = sectionName + ".cfg"
+                thisfolder = os.path.dirname(os.path.abspath(__file__))
+                initfile = os.path.join(thisfolder, cfgName)
+                config.read(initfile)
+                mounts = config.get(sectionName, 'mounts').split(",")
+                for mount in mounts:
+                    mountTarFileDes = mount.split()[1]
+                    os.chdir("../base_images/")
+                    # if len(mountTarFileDes.split("/")) == 3:
+                    os.system("umount -l $PWD/"+ sectionName + "/basefs" + mountTarFileDes)
+                    os.chdir("../src")
+                os.chdir("../base_images/")
+                os.system('chroot ./'+ sectionName + '/basefs /bin/bash -c "umount proc"')
+                os.system("rm -rf ./" + sectionName)
+                os.chdir("../src")
+                instances = [x for x in instances if x["instance"] != pid]
+                # instances.remove(instance)    ## just change it 
+                break
+                
+
+    # def kill_all_processes():
+    #     global processes
+    #     global instances
+    #     print(processes)
+    #     for process in processes:
+    #         pid = str(process.pid)
+	#     if (process.is_alive()):
+	# 	os.killpg(process.pid, signal.SIGKILL)
+    #     time.sleep(0.1)
+    #     print(process.is_alive())
+    #     print(processes)
+    #     for instance in instances:
+    #         if instance["instance"] == pid:
+    #             print("inside")
+    #             sectionName = instance["name"] + "-" + instance["major"] + "-" + instance["minor"]
+    #             config = configparser.RawConfigParser()
+    #             cfgName = sectionName + ".cfg"
+    #             thisfolder = os.path.dirname(os.path.abspath(__file__))
+    #             initfile = os.path.join(thisfolder, cfgName)
+    #             config.read(initfile)
+    #             mounts = config.get(sectionName, 'mounts').split(",")
+    #             for mount in mounts:
+    #                 mountTarFileDes = mount.split()[1]
+    #                 os.chdir("../base_images/")
+    #                 # if len(mountTarFileDes.split("/")) == 3:
+    #                 os.system("umount -l $PWD/"+ sectionName + "/basefs" + mountTarFileDes)
+    #                 os.chdir("../src")
+    #             os.chdir("../base_images/")
+    #             os.system('chroot ./'+ sectionName + '/basefs /bin/bash -c "umount proc"')
+    #             os.system("rm -rf ./" + sectionName)
+    #             os.chdir("../src")
+    #     processes.clear()
+    #     instances.clear()
+                    
